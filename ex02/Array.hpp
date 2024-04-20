9c935b2f3f3c09814eb82cd4de1602354df23269
rmitache <rmitache@student.42heilbronn.de>
2024-04-20 11:02:39 +0000

    Refactor Array class in ex02/Array.hpp and update main.cpp. More tests needed here.

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,26 +1,62 @@
 #pragma once
 
+#include <exception>
 #define ARRAY_HPP
 
 #include <iostream>
 
 template <typename T> class Array {
 private:
-	T*           array;
-	unsigned int size;
+	T*           _array;
+	unsigned int _size;
 
 public:
 	Array(void)
-		: size(0) {
-		this->array = new T;
+		: _size(0) {
+		this->_array = new T;
 		std::cout << "Default constructor called" << std::endl;
 	}
 
 	Array(unsigned int n)
-		: size(n) {
-		this->array = new T[n];
+		: _size(n) {
+		this->_array = new T[n];
+		for (unsigned int i = 0; i < n; i++) {
+			this->_array[i] = n;
+		}
 		std::cout << "Costructor with a size of " << n
 				  << " called" << std::endl;
 	}
+
+	Array(Array& copy)
+		: _array(copy._array), _size(copy._size) {
+		std::cout << "Copy constructor called" << std::endl;
+	}
+
+	Array& operator=(Array const& other) {
+		this->_array = other._array;
+		this->_size  = other._size;
+		std::cout << "Assign operator called" << std::endl;
+		return *this;
+	}
+
+	T& operator[](unsigned int index) {
+		if (index < 0 || index >= _size) {
+			throw outOfRangeException();
+		}
+		return (this->_array[index]);
+	}
+
+	class outOfRangeException : public std::exception {
+		const char* what() const throw() {
+			return "Out of Range of Array!\n";
+		}
+	};
+
+	unsigned int size() {
+		std::cout << "The size of the array is " << _size
+				  << std::endl;
+		return _size;
+	}
+
 	~Array() { std::cout << "Destructor called" << std::endl; }
 };
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,8 +1,19 @@
 #include "Array.hpp"
+#include <cstdlib>
+#include <iostream>
 
-#define MAX_VAL 150
+#define MAX_VAL 750
+#define ARRAY_SIZE 100
+#define INDEX_OUT_OF_RANGE 103
 
 int main() {
 	Array<int> numbers(MAX_VAL);
+	for (int i = 0; i < ARRAY_SIZE; i++) {
+		numbers[i] = rand();
+		std::cout << "numbers: " << numbers[i] << std::endl;
+	}
+	std::cout << "------ Out of Range Test ------\n"
+			  << std::endl;
+	numbers[INDEX_OUT_OF_RANGE];
 	return 0;
 }
